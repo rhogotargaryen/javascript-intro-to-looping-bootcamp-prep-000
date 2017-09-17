06bdf9b47e19f07387006574f16cc45144551eb3
Derek Haller <dhaller.personal@gmail.com>
2017-09-17 04:19:28 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,20 @@
+function forLoop(array) {
+  var t = array.length
+  for (let i = 0; i < 25; i++) {
+    if (i + t == t) {
+      array[i + t] = 'I am 1 strange loop.';
+    }
+    else {
+      array[i + t] = `I am ${i} strange loops.`;
+    }
+  }
+  return array;
+}
+
+function whileLoop(n) {
+  i = 0;
+  while (i <= n);
+  console.log(--n);
+  i++;
+  return 'done'
+}
